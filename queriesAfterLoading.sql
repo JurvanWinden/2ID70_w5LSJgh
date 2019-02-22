04d3f5abfdb60fb7614aa204c39edacd7c87ec4b
larsvl96 <l.j.p.v.lieshout@student.tue.nl>
2019-02-22 11:25:47 +0100

    Update queriesAfterLoading.sql

--- queriesAfterLoading.sql
+++ queriesAfterLoading.sql
@@ -1,5 +1,5 @@
 -- queries to run after loading + preparing database
 
 SELECT * FROM CourseOffers, Courses, Degrees, Teachers WHERE CourseOffers.courseOfferId = 1;
-SELECT CourseOfferId, CourseId, Year, Quartile, StudentId, StudentName, Address, BirthYearStudent, Gender, DegreeId, Dept, DegreeDescription, TotalECTS FROM StudentAssistants, CourseOffers, Students, Degrees WHERE StudentAssistents.StudenRegistrationId=140;
+SELECT StudentAssistants.CourseOfferId, CourseId, Year, Quartile, StudentId, StudentName, Address, BirthYearStudent, Gender, DegreeId, Dept, DegreeDescription, TotalECTS FROM StudentAssistants, CourseOffers, Students, Degrees WHERE StudentAssistents.StudenRegistrationId=140;
 SELECT AVG(Grade) FROM CourseRegistrations WHERE CourseRegistrations.StudentRegistrationId=140;
