c4172a2a456aaae0712a95b5daa52b30acc7c68f
larsvl96 <l.j.p.v.lieshout@student.tue.nl>
2019-02-27 13:50:33 +0100

    Update q2Queries.sql

--- q2Queries.sql
+++ q2Queries.sql
@@ -1,4 +1,13 @@
 --The 8 queries to be answered
+-- Q1
+SELECT CourseName, Grade
+FROM CourseRegistrations as cr, StudentRegistrationToDegrees as sd, CourseOffers as co, Courses as c
+WHERE 	StudentId = %1% AND DegreeId = %2% AND
+		sd.StudentRegistrationId = cr.StudentRegistrationId AND
+		cr.CourseOfferId = co.CourseOfferId AND
+		co.CourseId = c.CourseId AND
+		cr.Grade > 5
+GROUP BY (co.Year, co.Quartile, co.CourseOfferId);
 
 -- Q2
 -- Select all students that have completed a degree
@@ -20,6 +29,9 @@ AND Degrees.DegreeId = Courses.DegreeId
 AND Degrees.TotalECTS = 200
 AND NOT EGrade < 5
 
+-- Q3
+-- needed: all active students, percentage female
+
 --Q4 Give percentage of female students for all degrees of a department
 WITH StudentCount AS (
     SELECT COUNT(Students.StudentId) AS SC FROM Degrees, StudentRegistrationsToDegrees, Students
@@ -38,7 +50,19 @@ FemaleStudentCount AS (
 )
 SELECT (FSC / CAST(SC AS DECIMAL) * 100) AS Percentage FROM FemaleStudentCount, StudentCount;
 
+-- Q5
+-- needed: passing per course offer, total number of students, without course drop outs
+
 --Q6
+
+-- Q7
+SELECT DegreeId, BirthYearStudent, Gender, AVG(Grade)
+FROM CourseRegistrations as cr, CourseOffers as co, Courses as c, Students as s, StudentRegistrationToDegrees as sd
+WHERE 	cr.CourseOfferId = co.CourseOfferId AND
+		co.CourseId = c.CourseId AND
+		cr.StudentRegistrationId = sd.StudentRegistrationId AND
+		s.StudentId = sd.StudentId
+GROUP BY CUBE(DegreeId, BirthYearStudent, Gender);
 
 -- Q8
 -- Count Students for each CourseOffer
