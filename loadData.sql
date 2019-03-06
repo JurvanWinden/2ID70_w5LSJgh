48a9b76cbedb68af5ab1875687cd261331bcb157
Unknown <jur.vanwinden@gmail.com>
2019-03-06 16:14:15 +0100

    Auto stash before merge of "master" and "local"

--- loadData.sql
+++ loadData.sql
@@ -25,4 +25,36 @@ COPY StudentRegistrationsToDegrees(StudentRegistrationId, StudentId, DegreeId, R
 
 COPY Teachers(TeacherId, TeacherName, Address, BirthyearTeacher, Gender) FROM '/mnt/ramdisk/tables/Teachers.table' DELIMITER ',' CSV HEADER;
 
+ALTER TABLE Degrees ADD PRIMARY KEY (DegreeId);
+
+ALTER TABLE Students ADD PRIMARY KEY (StudentId);
+
+ALTER TABLE StudentRegistrationsToDegrees ADD PRIMARY KEY (StudentRegistrationId);
+
+ALTER TABLE Teachers ADD PRIMARY KEY (TeacherId);
+
+ALTER TABLE Courses ADD PRIMARY KEY (CourseId);
+
+ALTER TABLE CourseOffers ADD PRIMARY KEY (CourseOfferId);
+
+ALTER TABLE StudentRegistrationsToDegrees ADD FOREIGN KEY (StudentId) REFERENCES Students(StudentId);
+
+ALTER TABLE StudentRegistrationsToDegrees ADD FOREIGN KEY (DegreeId) REFERENCES Degrees(DegreeId);
+
+ALTER TABLE Courses ADD FOREIGN KEY (DegreeId) REFERENCES Degrees(DegreeId);
+
+ALTER TABLE CourseOffers ADD FOREIGN KEY (CourseId) REFERENCES Courses(CourseId);
+
+ALTER TABLE TeacherAssignmentsToCourses ADD FOREIGN KEY (CourseOfferId) REFERENCES CourseOffers(CourseOfferId);
+
+ALTER TABLE TeacherAssignmentsToCourses ADD FOREIGN KEY (TeacherId) REFERENCES Teachers(TeacherId);
+
+ALTER TABLE StudentAssistants ADD FOREIGN KEY (CourseOfferId) REFERENCES CourseOffers(CourseOfferId);
+
+ALTER TABLE StudentAssistants ADD FOREIGN KEY (StudentRegistrationId) REFERENCES StudentRegistrationsToDegrees(StudentRegistrationId);
+
+ALTER TABLE CourseRegistrations ADD FOREIGN KEY (CourseOfferId) REFERENCES CourseOffers(CourseOfferId);
+
+ALTER TABLE CourseRegistrations ADD FOREIGN KEY (StudentRegistrationId) REFERENCES StudentRegistrationsToDegrees(StudentRegistrationId);
+
 ANALYZE VERBOSE;
