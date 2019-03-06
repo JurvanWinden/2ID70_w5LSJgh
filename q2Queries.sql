f6f29b6bb432e5f100765691e0daa328680bbee0
Unknown <jur.vanwinden@gmail.com>
2019-03-06 17:07:32 +0100

    Added Q1

--- q2Queries.sql
+++ q2Queries.sql
@@ -1,4 +1,4 @@
-SELECT 0;
+SELECT CourseName, Grade FROM PassedCoursesPerStudent AS P, Courses AS C WHERE P.StudentId = %1% AND SD.DegreeId = %2% AND P.CourseId = C.CourseId;
 SELECT 0;
 SELECT 0;
 WITH StudentCount AS (SELECT COUNT(Students.StudentId) AS SC FROM Degrees, StudentRegistrationsToDegrees, Students WHERE Students.StudentId = StudentRegistrationsToDegrees.StudentId AND StudentRegistrationsToDegrees.DegreeId = Degrees.DegreeId AND Dept = %1% GROUP BY Degrees.Dept ), FemaleStudentCount AS ( SELECT COUNT(Students.StudentId) AS FSC FROM Degrees, StudentRegistrationsToDegrees, Students WHERE Students.StudentId = StudentRegistrationsToDegrees.StudentId AND StudentRegistrationsToDegrees.DegreeId = Degrees.DegreeId AND Gender = 'F' AND Degrees.Dept = %1% GROUP BY Degrees.Dept) SELECT (FSC / CAST (SC AS DECIMAL) ) AS Percentage FROM FemaleStudentCount, StudentCount;
